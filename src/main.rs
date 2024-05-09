f8cbc1b2cfc8ce652bc59f9af89497811b0050db
AuTsing <763025696@qq.com>
2024-05-10 02:54:14 +0800

    add main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("TARGET", "aarch64-linux-android");
+    env::set_var("HOST", "aarch64-linux-android");
+    env::set_var("PROFILE", "release");
+    env::set_var("OUT_DIR", "./out");
+    env::set_var("CARGO_MANIFEST_DIR", "./src/deno_builder");
+    deno_builder::main();
 }
