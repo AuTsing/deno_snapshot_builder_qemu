7ef549c14e0bcd5d33ad128b9fc27a253cdb3f9c
AuTsing <763025696@qq.com>
2024-05-11 02:52:43 +0800

    fix step Build snapshot

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ fn main() {
     let out_dir = working_dir.join("out");
     let cargo_manifest_dir = working_dir.join("src/deno_builder/deno/cli");
 
+    println!("out_dir: {:?}", &out_dir);
+    println!("cargo_manifest_dir: {:?}", &cargo_manifest_dir);
+
     env::set_current_dir(&cargo_manifest_dir).unwrap();
     env::set_var("TARGET", "aarch64-linux-android");
     env::set_var("HOST", "aarch64-linux-android");
