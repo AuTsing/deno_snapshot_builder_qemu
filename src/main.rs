9838c74268d0f961e1c38bed31086742c3b5a440
AuTsing <763025696@qq.com>
2024-05-11 02:37:14 +0800

    fix step Build snapshot

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::env;
 
 fn main() {
+    let working_dir = env::current_dir().unwrap();
+    let out_dir = working_dir.join("out");
+    let cargo_manifest_dir = working_dir.join("src/deno_builder/deno/cli");
+
+    env::set_current_dir(&cargo_manifest_dir).unwrap();
     env::set_var("TARGET", "aarch64-linux-android");
     env::set_var("HOST", "aarch64-linux-android");
     env::set_var("PROFILE", "release");
-    env::set_var("OUT_DIR", "./out");
-    env::set_var("CARGO_MANIFEST_DIR", "./src/deno_builder");
+    env::set_var("OUT_DIR", &out_dir);
+    env::set_var("CARGO_MANIFEST_DIR", &cargo_manifest_dir);
     deno_builder::main();
 }
