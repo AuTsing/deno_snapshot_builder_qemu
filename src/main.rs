ff1e6781e78eb5905cdb55e6b2d87fb59fed4636
AuTsing <763025696@qq.com>
2024-05-13 02:43:07 +0800

    fix step Build

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,16 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // let working_dir = env::current_dir().unwrap();
-    // let out_dir = working_dir.join("out");
-    // let cargo_manifest_dir = working_dir.join("src/deno_builder");
+    let working_dir = env::current_dir().unwrap();
+    let out_dir = working_dir.join("out");
+    let cargo_manifest_dir = working_dir.join("src/deno_builder");
 
-    // env::set_current_dir(&cargo_manifest_dir).unwrap();
-    // fs::create_dir(&out_dir).unwrap();
-    // env::set_var("TARGET", "aarch64-linux-android");
-    // env::set_var("HOST", "aarch64-linux-android");
-    // env::set_var("PROFILE", "release");
-    // env::set_var("OUT_DIR", &out_dir);
-    // env::set_var("CARGO_MANIFEST_DIR", &cargo_manifest_dir);
-    // deno_builder::main();
-
-    println!("hello world");
+    env::set_current_dir(&cargo_manifest_dir).unwrap();
+    fs::create_dir(&out_dir).unwrap();
+    env::set_var("TARGET", "aarch64-linux-android");
+    env::set_var("HOST", "aarch64-linux-android");
+    env::set_var("PROFILE", "release");
+    env::set_var("OUT_DIR", &out_dir);
+    env::set_var("CARGO_MANIFEST_DIR", &cargo_manifest_dir);
+    deno_builder::main();
 }
